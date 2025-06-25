8b33ab39221e314bf9e66f000f4d3e313ed1f4f5
BadhonAhmadDev <badhongithub@gmail.com>
2025-06-25 16:50:27 +0600

    Update server.js for Render deployment

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const audioFilePath = path.join(__dirname, 'audio.mp3');
 
 // Telegram config
 const TELEGRAM_BOT_TOKEN = '7909147905:AAH9-pbLUfkTf-YYNj9peiFEvAbeWAAFPro';
-const TELEGRAM_CHAT_ID = '-1002706453375';
+// const TELEGRAM_CHAT_ID = '-1002706453375';
+const TELEGRAM_CHAT_ID = '5440260132';
+
 
 async function sendTelegramTo(chatId, text) {
   const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
